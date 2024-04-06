b2df4fc1e942fa063ac1b2a6e9c8000f0f5a9ef9
Alma <almastar405@gmail.com>
2024-04-06 18:00:34 -0500

    example code

---
+++ Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+
+    private Rigidbody2D rigidbody2;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2 = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float HInput = Input.GetAxis("Horizontal");
+        float VInput = Input.GetAxis("Vertical");
+
+        rigidbody2.velocity = new Vector2(HInput, VInput) * speed;
+
+    }
+}
