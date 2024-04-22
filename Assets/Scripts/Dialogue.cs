67e1a8a418b947708253e87027e56553cec27e37
audreydieuanh <trinhdieu@grinnell.edu>
2024-04-22 13:47:22 -0500

    Try adding dialogue

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : ScriptableObject
+{
+    public string[] lines;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
