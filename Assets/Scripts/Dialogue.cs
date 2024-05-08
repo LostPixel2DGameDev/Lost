8666c79a4a0832bf866dd8d3eeeb1f889eb535af
audreydieuanh <trinhdieu@grinnell.edu>
2024-05-08 12:36:02 -0500

    Add chest

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Dialogue/New Dialogue Container")]
 public class Dialogue : ScriptableObject
 {
-    public string[] lines;
+    public string speakerName;
+
+    [TextArea(5, 10)]
+    public string[] paragraphs;
     // Start is called before the first frame update
     void Start()
     {
